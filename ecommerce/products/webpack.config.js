0bdff02f001f4a42ccdf27a0e1693debe01d9663
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-08-14 14:17:13 +0530

    initial setup of product project
    webpack setup with faker data

---
+++ ecommerce/products/webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
