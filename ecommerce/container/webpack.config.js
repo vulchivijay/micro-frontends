b6ec6e0f5e1dd9638dd4f0232e2f2afd325e278e
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-08-14 16:02:25 +0530

    comments added for webpack config

--- ecommerce/container/webpack.config.js
+++ ecommerce/container/webpack.config.js
@@ -8,13 +8,16 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
+      name: 'container', // not used, added for clarity. only needed for remotes
+      remotes: { // lists projects that the container can search to get additional code
         products: 'products@http://localhost:8081/remoteEntry.js'
+        // products: load the file at the listed url if anything in container has an import like import abc from products
+        // url - products: related to the 'name' property in the product webpack config file
+        // url - url: url for the remoteEntry file.
       }
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html' // starting point
     })
   ]
 }
--- ecommerce/products/webpack.config.js
+++ ecommerce/products/webpack.config.js
@@ -8,14 +8,14 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'products',
-      filename: 'remoteEntry.js',
+      name: 'products', // more on this 'name' in a moment
+      filename: 'remoteEntry.js', // sets the name of the manifest file. leave it as remoteEntry.js unless you have got a good reason to change it.
       exposes: {
-        './ProductIndex': './src/index.js'
+        './ProductIndex': './src/index.js' // aliases filenames
       }
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html' // starting point
     })
   ]
 };
